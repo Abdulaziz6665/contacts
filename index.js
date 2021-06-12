9c6f59f212fac46e972db3b0ba793858ca189fb3
Abdulaziz6665 <mr.yunusobod@gmail.com>
2021-06-12 18:07:51 +0500

    sss

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4600
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 // app.use(express.static('public'))
 
 // app.use((req, res, next) => {
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, './client/build')))
 // console.log(path.join(__dirname, 'client/build'))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 const SQL = `
